aff55e3df1d97e40cb825b3fb7d1a95ed8d406a6
Lucas KPS <lucas.12.kps@gmail.com>
2025-10-06 17:50:10 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-397848186-8ac18",
+  "appId": "1:486015888893:web:406ab4521fa85134077d94",
+  "apiKey": "AIzaSyBEvUNEkpkQRpyUhMfEFbWBKJiRtYddNO8",
+  "authDomain": "studio-397848186-8ac18.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "486015888893"
+};
